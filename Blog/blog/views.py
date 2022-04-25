96e1a0455ddb416314ca93d741581c791a138c03
Ali Al-Thiab <alialthiab1998@gmail.com>
2022-04-25 11:28:44 +0300

    Inetial Project Upload

---
+++ Blog/blog/views.py
@@ -0,0 +1,89 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Post
+from django.views.generic import (
+                        ListView,
+                        DetailView,
+                        CreateView,
+                        UpdateView,
+                        DeleteView
+)
+
+
+def home(request):
+
+    return render(request, 'blog/home.html', {'posts': Post.objects.all()})
+
+
+def about(request):
+    return render(request, 'blog/about.html',  {'posts': posts})
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    # no need to import class Paginator
+    paginate_by = 5 #5 for num of posts
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    # no need to import class Paginator
+    paginate_by = 5 #5 for num of posts
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView, LoginRequiredMixin):
+    model = Post
+    fields = ['title', 'content']
+
+    #setting the author feild
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form) # for set the author in the parent class
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    #setting the author feild
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form) # for set the author in the parent class
+
+# Error message for not update posts except the author
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'# redirect to home page
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)  # for set the author in the parent class
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
---
+++ Blog/users/forms.py
@@ -0,0 +1,34 @@
+from typing import List
+
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+'''
+create form to update user and profile in the same form
+'username', 'email', 'image'
+'''
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
